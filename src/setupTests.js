1ae5ece9595b574e65cda918b866ee8024b177f1
liuyang <liuyangc33@gmail.com>
2018-12-21 11:16:29 +0800

    support fetch in jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,27 @@
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn(),
-};
-global.localStorage = localStorageMock;
+import { Headers } from 'node-fetch';
+
+
+const localStorageMock = (function() {
+  let store = {};
+  return {
+    getItem: function(key) {
+      return store[key] || null
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString()
+    },
+    removeItem: function(key) {
+      delete store[key]
+    },
+    clear: function() {
+      store = {}
+    },
+  }
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
+Object.defineProperty(window, 'Headers', {
+  value: Headers,
+});
